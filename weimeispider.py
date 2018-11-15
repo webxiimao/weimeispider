9af2b46b0ff6fb17ad530462e7c0a64fe97e8e37
webxiimao <web_xiimao@outlook.com>
2018-11-15 18:10:22 +0800

    完成二层爬取

--- weimeispider.py
+++ weimeispider.py
@@ -1,5 +1,6 @@
 #-*- coding=gbk -*-
 import requests
+import re
 from lxml import html
 from dbqueue import create_connect
 from Download import request
@@ -18,20 +19,57 @@ def weimeispider():
     # #获取元素
     tags = tag_tree.xpath("//div[@class='TagList'][1]/a")
     urls = handle_tags(tags)
-    # print(urls)
-    # for url in urls:
-    #     album_handle(url)
 
 
-def album_handle(url):
+
+def album_handle(url,tag):
     '''
     相册表的处理
     :param url:
     :return:
     '''
+    #tag 插入
     album_tree = request_url(url)
-    # album = album_tree.xpath("")
 
+    tag_title = album_tree.xpath('//div[@class="TagTxt"]/p')[0]
+    cursor = connect.cursor()
+    sql = "insert into girls_tag(title,tag) values(%s,%s)"
+    cursor.execute(sql, [tag_title.text,tag.text])
+    id = int(cursor.lastrowid)
+    cursor.close()
+    connect.commit()
+    #全页数处理逻辑
+    max = get_all_album_page(album_tree)
+    #album逻辑处理
+
+    albums = album_tree.xpath("//div[@class='ABox']//a")
+    for album in albums:
+        title = album.xpath('img/@alt')[0]
+        url = album.xpath('@href')[0]
+        cover = album.xpath('img/@src')[0]
+        img_handle(url, title, cover ,id)
+
+def img_handle(url, title, cover ,id):
+    img_tree = request_url(url)
+
+    album_discription = img_tree.xpath("//div[@class='descriptionBox']/p")[0].text if len(img_tree.xpath("//div[@class='descriptionBox']/p"))>0 else ""
+    cursor = connect.cursor()
+    sql = "insert into girls_album(title,cover_img,description,girls_tag_id) values(%s,%s,%s,%s)"
+    cursor.execute(sql, [title, cover, album_discription ,id])
+    id = int(cursor.lastrowid)
+    cursor.close()
+    connect.commit()
+
+
+
+def get_all_album_page(album_tree):
+    last_page = album_tree.xpath("//div[@class='wrappic']/div[@class='pages']/ul/li")[-1]
+    max_a = last_page.xpath("a")
+    max = 0
+    if len(max_a) > 0:
+        max_page = max_a[0].xpath('@href')[0]
+        max = re.match(r'\S+/(\d+).html', max_page).group(1)
+    return max
 
 
 def request_url(url):
@@ -49,15 +87,8 @@ def handle_tags(tags):
     #处理tags
     for tag in tags:
         # 获取url
-        cursor = connect.cursor()
-        # tags_href.append("%s%s"%(base_url,tag.xpath("@href")[0]))
-        # 获取tag名
-        # tags_text.append(tuple([tag.text]))
-        sql = "insert into girls_tag(tag) values(%s)"
-        cursor.execute(sql, [tag.text])
-        cursor.close()
-        album_handle("%s%s" % (base_url, tag.xpath("@href")[0]))
-    connect.commit()
+        album_handle("%s%s" % (base_url, tag.xpath("@href")[0]), tag)
+
     connect.close()
 
 def sql_run(sql, params=None):
